9be1c5955f265d279da13d5955e95527ac997be8
kamenskymic <105462842+kamenskymic@users.noreply.github.com>
2023-08-06 11:33:36 +0300

    Create ex23-02_dbg_source_join.js

---
+++ ex23-02_dbg_source_join/ex23-02_dbg_source_join.js
@@ -0,0 +1,27 @@
+
+if (window.location.search == "")
+{
+    location.href = location.href + "?param1=%22%20onerror%3Dalert(1)%3E";
+}
+
+// The join mode rule should only highlight this result
+// ruleid: ex23-02_dbg_source_join
+function GetSomeData1()
+{
+    return decodeURI(new URLSearchParams(window.location.search).get('param1'));
+}
+
+// The join mode rule should not highlight this result
+// ok: ex23-02_dbg_source_join
+function GetSomeData2()
+{
+    return "Safe hardcoded data";
+}
+
+// The join mode rule should not highlight this result
+// ruleid: ex23-02_dbg_source_join
+function GetSomeData3()
+{
+    // Not actually used
+    return decodeURI(new URLSearchParams(window.location.search).get('param3'));
+}
